d4fad3b188f76f2eb1968874cc666da54af86738
Fadzi Ushewokunze <kingfadzi@gmail.com>
2025-03-12 14:43:34 -0400

    create user

---
+++ env_vars.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export DB_USER="postgres"
+export DB_PASS="postgres"
+export DB_HOST="192.168.1.188"
+export DB_PORT="5432"
+export DB_NAME="superset"
+
+export REDIS_HOST="192.168.1.188"
+export REDIS_PORT="6379"
+export REDIS_DB="0"
+
+export MB_DB_TYPE="postgres"
+export MB_DB_DBNAME="metabase"
+export MB_DB_PORT="5432"
+export MB_DB_USER="postgres"
+export MB_DB_PASS="postgres"
+export MB_DB_HOST="192.168.1.188"
+export MB_JETTY_HOST="0.0.0.0"
